ea608f6ca69c8bff8c5debd84b72b076a9ddbd58
Maxim Vorobjov <maxim.vorobjov@gmail.com>
2020-05-08 14:18:11 +0300

    Functions trait

--- node/src/template/mod.rs
+++ node/src/template/mod.rs
@@ -1,18 +1,20 @@
 use crate::{
     db::models::{AssetState, DigitalAsset},
-    types::{AssetID, TemplateID},
+    types::{AssetID, TemplateID, TokenID},
 };
 use serde_json::Value;
 use std::str::FromStr;
 
+pub trait Functions: FromStr {}
+
 #[async_trait::async_trait]
 pub trait Template {
-    type Error: std::error::Error;
-    type AssetFunctions: FromStr<Err = Self::Error>;
-    type TokenFunctions: FromStr<Err = Self::Error>;
+    type Error;
+    type AssetFunction: Functions;
+    type TokenFunction: Functions;
 
     fn id() -> TemplateID;
     async fn create_asset(asset: DigitalAsset, state: AssetState) -> Result<AssetState, Self::Error>;
-    async fn asset_call(function: Self::AssetFunctions, asset_id: AssetID, params: Value);
-    async fn token_call(function: Self::TokenFunctions, token_id: TokenID, params: Value);
+    async fn asset_call(function: Self::AssetFunction, asset_id: AssetID, params: Value);
+    async fn token_call(function: Self::TokenFunction, token_id: TokenID, params: Value);
 }
