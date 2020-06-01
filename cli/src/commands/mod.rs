de9e5d04b2d3a1538b048553f91b03815ef1d348
Maxim Vorobjov <maxim.vorobjov@gmail.com>
2020-06-01 18:10:08 +0300

    add todoes agreed during planning

--- cli/src/commands/mod.rs
+++ cli/src/commands/mod.rs
@@ -25,6 +25,14 @@ pub enum Commands {
     Template(TemplateCommands),
     /// Work with template
     Asset(AssetCommands),
+    // TODO: Demo: cargo run  -- token list'
+    // TODO: Demo: cargo run  -- token view <token_id>'
+    // TODO: Demo: cargo run  -- instruction asset 0000000100000000000000000000000.0000000000000000000000 issue_tokens
+    // --data '{"number": 6}' TODO: Demo: cargo run  -- instruction token sell_token --data '{"owner_pubkey":
+    // pubkey, "price": 100.0, "timeout": }' --autopick walletPubkey, token_id
+    // TODO: Demo: cargo run  -- instruction wallet set_balance walletPubkey 100.0
+    // TODO: Demo: cargo run  -- instruction token redeem_token
+    // 0000000100000000000000000000000.0000000000000000000000.000000000..01
     /// Recreate and migrate database,  *DANGER!* it will wipe all data
     Wipe {
         /// Don't prompt for confirmation
